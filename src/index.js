4a40b3ae6b99d0abc89c141991129f0cbe818334
James Kent <jkent@chariotsolutions.com>
2020-04-15 11:33:05 -0400

    uses useFieldArray append rather than setValue

--- src/index.js
+++ src/index.js
@@ -1,73 +1,65 @@
 import "./styles.css";
-import React, {useEffect} from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import {findFileExtension} from "./tools";
+import {useDropzone} from "react-dropzone";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import {HighlightOff} from "@material-ui/icons";
+import {HelpOutline, HighlightOff} from "@material-ui/icons";
+import Avatar from "@material-ui/core/Avatar";
 import TextField from "@material-ui/core/TextField";
 import {useFieldArray, useForm} from "react-hook-form";
 import IconButton from "@material-ui/core/IconButton";
 
 
 function App() {
-  const {setValue, control, register, handleSubmit, errors, watch, getValues} = useForm();
-  const {fields, remove} = useFieldArray({control, name: "files"});
-  const watchedFiles = watch('files', []);
+  const methods = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control: methods.control,
+    name: "files"
+  });
 
-  const handleChange = (e) => {
-    const file = e.target.files[0]; // only taking one file for the time being
-    const transformed = [{
+  const {getInputProps, getRootProps} = useDropzone({
+    onDrop: files => files.forEach(file => append({
       name: file.name,
       preview: URL.createObjectURL(file),
       extension: findFileExtension(file),
       file: file,
-    }];
-    console.log("handleChange", transformed)
-    setValue('files', transformed);
-  };
-
-  useEffect(() => {
-    register({name: "files"}, {required: true}) // still have validation for required
-  }, [register]);
+    })),
+  });
 
   const submit = data => {
-    console.log("submitted data", data);
-    console.log("this should be defined", data.files[0].file.name)
+    console.log("submitted data", data, fields);
+    console.log("this should be defined", data.files[0].file.name, fields[0].file.name)
   };
 
-  console.log("watchedFiles", watchedFiles, "fields", fields, "getValues", getValues())
-
   return (
-    <form onSubmit={handleSubmit(submit)}>
-      {watchedFiles.map((f, index) => (
+    <form onSubmit={methods.handleSubmit(submit)}>
+      {fields.map((f, index) => (
         <Box
           display={"flex"}
           alignItems={"center"}
           key={f.name}
         >
-          {/*<Avatar alt={f.file.name} src={f.preview}>*/}
-          {/*  {f.extension ?? <HelpOutline/>}*/}
-          {/*</Avatar>*/}
-          <TextField
-            variant={"filled"}
-            label={"Name"}
-            name={`files[${index}].name`}
-            inputRef={register({required: "this is required"})}
-          />
+          <Avatar alt={f.name} src={f.preview}>
+            {f.extension ?? <HelpOutline/>}
+          </Avatar>
+          <Box m={1}>{f.name}</Box>
           <TextField
             variant={"filled"}
             label={"Metadata"}
             name={`files[${index}].metadata`}
-            inputRef={register({required: "this is required"})}
-            error={Boolean(errors.files
-              && errors.files[index]
-              && errors.files[index].metadata)}
-            helperText={errors.files
-              && errors.files[index]
-              && errors.files[index].metadata
-              && errors.files[index].metadata.message}
+            inputRef={methods.register({required: "this is required"})}
+            error={Boolean(methods.errors.files
+              && methods.errors.files[index]
+              && methods.errors.files[index].metadata)}
+            helperText={methods.errors.files
+            && methods.errors.files[index]
+            && methods.errors.files[index].metadata.message}
           />
-          <input type={"hidden"} name={`files[${index}].file`} ref={register()}/>
+          <input type={"hidden"} name={`files[${index}].file`} ref={methods.register()} defaultValue={f.file}/>
+          <input type={"hidden"} name={`files[${index}].name`} ref={methods.register()} defaultValue={f.name}/>
 
           <Box color={"error.main"}>
             <IconButton
@@ -80,14 +72,17 @@ function App() {
         </Box>
       ))}
 
-      <input
-        type={"file"}
-        name={"files"}
-        multiple
-        onChange={handleChange}
-      />
+      <div {...getRootProps()}>
+        <input {...getInputProps()}/>
+        <Typography variant="body1" fontWeight="bold">
+          Drag and drop files here or
+        </Typography>
+        <Button color="primary">
+          Click Here to Upload
+        </Button>
+      </div>
 
-      <input type={"submit"} disabled={watchedFiles.length === 0}/>
+      <input type={"submit"} disabled={fields.length === 0}/>
     </form>
   )
 }
